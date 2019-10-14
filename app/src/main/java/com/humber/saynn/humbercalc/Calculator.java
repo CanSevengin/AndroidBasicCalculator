3ac3c40f22861a769db64bae29a570d77fb4ed00
Sinan <syaman14@ku.edu.tr>
2019-10-14 13:54:51 -0400

    All done except error handling.

--- app/src/main/java/com/humber/saynn/humbercalc/Calculator.java
+++ app/src/main/java/com/humber/saynn/humbercalc/Calculator.java
@@ -3,6 +3,8 @@ package com.humber.saynn.humbercalc;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
@@ -14,29 +16,34 @@ public class Calculator {
     boolean newOp;
     DecimalFormat df;
     boolean percentage;
-    // QUIZ NOTE : PERCENTAGE IMPLEMENTATION IS IN LINE 109.
+    String memory;
+
     public Calculator() {
         resultString = "";
         historyString = "";
         stack = new ArrayList<String>();
         calculationStack = new ArrayList<String>();
         newOp = false;
-        df = new DecimalFormat("#.########");
+        df = new DecimalFormat("#.#######");
         percentage = false;
+        memory = "0";
     }
 
     public void numberClicked(String numberString) {
         String op = "";
+        // If the button clicked is a number
         if (isNumber(numberString)) {
+            //If this is not a start to a new operation
             if (!newOp) {
                 if (stack.size() != 0) {
+                    //If there is an arithmetic operation button is clicked beforehand
                     if (isArithmeticOperation(stack.get(stack.size() - 1))) {
                         this.resultString = numberString;
                         stack.remove(stack.size() - 1);
-                    } else {
+                    } else { //regular entry, concatenate with the existing string
                         this.resultString += numberString;
                     }
-                } else {
+                } else { // The very first entry
                     this.resultString += numberString;
                 }
             } else {
@@ -49,15 +56,75 @@ public class Calculator {
             // If any arithmetic operation is clicked
         } else if (isArithmeticOperation(numberString)) {
             //If no other calculation is made beforehand
-            Log.e("arit", "started: " + stack.toString());
+            op = getArithmeticOperation(numberString);
+            //CALL ARITHMETIC
+            doTheArithmetic(numberString, op);
+            //If functions are clicked
+            //CALL FUNCTION
+        }else if (isFunctionalButton(numberString)) {
+            String fnctn = getFunctionalButton(numberString);
+            doTheFunction(fnctn);
+        }
+    }
+
+    private void doTheFunction(String fnctn){
+        if (fnctn.equalsIgnoreCase(".")) {
+            if (!this.resultString.contains(".")) {
+                this.resultString += fnctn;
+            }
+        } else if (fnctn.equalsIgnoreCase("delete")) {
+            this.resultString = "";
+            this.historyString = "";
+            stack.clear();
+            calculationStack.clear();
+            newOp = true;
+        } else if (fnctn.equalsIgnoreCase("change")) {
             if (!this.resultString.isEmpty()) {
+                if (Double.valueOf(this.resultString) > 0) {
+                    this.resultString = "-" + this.resultString;
+                } else {
+                    this.resultString = Math.abs(Double.valueOf(this.resultString)) + "";
+                }
+            }
+            //CALLED IF PERCENTAGE IS CLICKED
+        } else if (fnctn.equalsIgnoreCase("percent")) {
+            percentage = true;
+            Double currentValue = Double.valueOf(this.resultString);
+            String percentValue = df.format(currentValue / 100);
+            if (!this.resultString.contains("%")) {
+                this.resultString += "%";
+            }
+            if (calculationStack.size() == 2) {
+                this.historyString = calculationStack.get(0) + " " + calculationStack.get(1) + " " + this.resultString;
+                String secondaryResult = doTheMath(calculationStack.get(0), percentValue, calculationStack.get(1));
+                this.resultString = secondaryResult;
+                newOp = true;
+                calculationStack.clear();
+                stack.clear();
+            }else{
+                this.resultString = percentValue;
+                percentage = false;
+            }
+        } else if (fnctn.equalsIgnoreCase("mr")) {
+            this.resultString = memory;
+
+        } else if (fnctn.equalsIgnoreCase("mc")) {
+            this.historyString = "Memory cleared. Memory: 0";
+            this.memory = "0";
+        }
+    }
+
+    private void doTheArithmetic(String numberString, String op){
+        Log.e("arit", "Arithmetic, OP: " + op);
+        if (!this.resultString.isEmpty()) {
+            if (!op.equalsIgnoreCase("mplus") && !op.equalsIgnoreCase("mminus")) {
                 stack.add(this.resultString);
                 calculationStack.add(this.resultString);
                 this.historyString = this.resultString;
                 this.resultString = "";
-                op = getArithmeticOperation(numberString);
                 //If equals is clicked
                 if (op.equalsIgnoreCase("equals")) {
+                    //Log.e("arit","Stack: " + stack.toString() + ", Calc: " + calculationStack.toString());
                     calculationStack.add(stack.get(stack.size() - 1));
                     //Log.e("arit","inside Equals: " + calculationStack.toString());
                     String newStackStarted = doTheMath(calculationStack.get(0), calculationStack.get(2), calculationStack.get(1));
@@ -82,64 +149,23 @@ public class Calculator {
                     calculationStack.add(op);
                     stack.add(numberString);
                 }
-            }
-        }
-        //If functions are clicked
-        if (isFunctionalButton(numberString)) {
-            String fnctn = getFunctionalButton(numberString);
-            if (fnctn.equalsIgnoreCase(".")) {
-                if (!this.resultString.contains(".")) {
-                    this.resultString += fnctn;
-                }
-
-            } else if (fnctn.equalsIgnoreCase("delete")) {
-                this.resultString = "";
-                this.historyString = "";
-                stack.clear();
-                calculationStack.clear();
-                newOp = true;
-            } else if (fnctn.equalsIgnoreCase("change")) {
-                if (!this.resultString.isEmpty()) {
-                    if (Double.valueOf(this.resultString) > 0) {
-                        this.resultString = "-" + this.resultString;
-                    } else {
-                        this.resultString = Math.abs(Double.valueOf(this.resultString)) + "";
-                    }
-                }
-                //CALLED IF PERCENTAGE IS CLICKED
-            } else if (fnctn.equalsIgnoreCase("percent")) {
-                percentage = true;
-                Double currentValue = Double.valueOf(this.resultString);
-                String percentValue = df.format(currentValue/100);
-                if(!this.resultString.contains("%")){
-                    this.resultString +="%";
-                }
-                if(calculationStack.size()==2){
-                    Log.e("arit",calculationStack.toString());
-                    String secondaryResult = doTheMath(calculationStack.get(0),percentValue,calculationStack.get(1));
-                    this.resultString=secondaryResult;
-                    newOp = true;
-                    calculationStack.clear();
-                    stack.clear();
-                    calculationStack.add(secondaryResult);
-                    stack.add(secondaryResult);
-                }
-
+            } else { //If memory function
+                doTheMath(memory, this.resultString, op);
             }
         }
     }
 
-
+    // Calculating
     private String doTheMath(String no1, String no2, String op) {
         String result = "";
         double number1 = Double.valueOf(no1);
         double number2 = Double.valueOf(no2);
         double res = 0;
         if (op.equalsIgnoreCase("plus")) {
-            if(percentage){
-                res = number1 + number2*number1;
+            if (percentage) {
+                res = number1 + number2 * number1;
                 percentage = false;
-            }else{
+            } else {
                 res = number1 + number2;
             }
             result = df.format(res);
@@ -147,32 +173,56 @@ public class Calculator {
             res = number1 * number2;
             result = df.format(res);
         } else if (op.equalsIgnoreCase("divide")) {
-            if(number2!=0){
+            if (number2 != 0) {
                 res = number1 / number2;
                 result = df.format(res);
             }
-
         } else if (op.equalsIgnoreCase("minus")) {
-            if(percentage){
-                res = number1 - number2*number1;
+            if (percentage) {
+                res = number1 - number2 * number1;
                 percentage = false;
-            }else{
+            } else {
                 res = number1 - number2;
             }
+            result = df.format(res);
+        } else if (op.equalsIgnoreCase("mplus")) {
+            if (!memory.isEmpty()) {
+                res = number1 + number2;
+                this.memory = df.format(res);
+                result = df.format(res);
+                this.historyString = this.resultString + " M+ ";
+                this.resultString = "";
+                newOp = true;
+            }
+        } else if (op.equalsIgnoreCase("mminus")) {
+            if (!memory.isEmpty()) {
+                res = number1 - number2;
+                this.memory = df.format(res);
+                result = df.format(res);
+                this.historyString = this.resultString + " M- ";
+                this.resultString = "";
+                newOp = true;
+            }
+        } else if(op.equalsIgnoreCase("power")){
+            res = Math.pow(number1,number2);
             result = df.format(res);
         }
         return result;
     }
 
+    // Long version of enuming, identifies the functional buttons
     private boolean isFunctionalButton(String numberString) {
         ArrayList<String> functionals = new ArrayList<String>();
         functionals.add("+-");
         functionals.add("C");
         functionals.add("%");
         functionals.add(".");
+        functionals.add("MR");
+        functionals.add("MC");
         return functionals.contains(numberString);
     }
 
+    // Long version of enuming, identifies the arithmetical buttons
     private boolean isArithmeticOperation(String s) {
         ArrayList<String> arithmetics = new ArrayList<String>();
         arithmetics.add("+");
@@ -180,17 +230,18 @@ public class Calculator {
         arithmetics.add("x");
         arithmetics.add("-");
         arithmetics.add("=");
+        arithmetics.add("M+");
+        arithmetics.add("M-");
+        arithmetics.add("\u2093\u02b8");
         return arithmetics.contains(s);
     }
 
+    //Checks if the button entered is number
     private boolean isNumber(String numberString) {
         char c = numberString.charAt(0);
-        if (Character.isDigit(c)) {
-            return true;
-        }
-
-        return false;
+        return Character.isDigit(c);
     }
+
 
     private String getArithmeticOperation(String s) {
         String op = "";
@@ -200,11 +251,16 @@ public class Calculator {
             op = "plus";
         } else if (s.equalsIgnoreCase("/")) {
             op = "divide";
-
         } else if (s.equalsIgnoreCase("-")) {
             op = "minus";
         } else if (s.equalsIgnoreCase("=")) {
             op = "equals";
+        } else if (s.equalsIgnoreCase("M+")) {
+            op = "mplus";
+        } else if (s.equalsIgnoreCase("M-")) {
+            op = "mminus";
+        } else if (s.equalsIgnoreCase("\u2093\u02b8")) {
+            op = "power";
         }
         return op;
     }
@@ -219,6 +275,10 @@ public class Calculator {
             fn = ".";
         } else if (s.equalsIgnoreCase("C")) {
             fn = "delete";
+        } else if (s.equalsIgnoreCase("MR")) {
+            fn = "mr";
+        } else if (s.equalsIgnoreCase("MC")) {
+            fn = "mc";
         }
         return fn;
     }
